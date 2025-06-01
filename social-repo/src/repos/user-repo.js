7f6703c79b7fd79c8ac4925a44d9957ee87a9019
Mike <spormuv@gmail.com>
2025-06-01 08:49:43 +0300

    feat: add testing. Finish project

--- social-repo/src/repos/user-repo.js
+++ social-repo/src/repos/user-repo.js
@@ -44,6 +44,12 @@ class UserRepo {
 
     return toCamelCase(rows)[0];
   }
+
+  static async count() {
+    const { rows } = await pool.query('SELECT COUNT(*) FROM users;');
+
+    return parseInt(rows[0].count);
+  }
 }
 
 module.exports = UserRepo;
